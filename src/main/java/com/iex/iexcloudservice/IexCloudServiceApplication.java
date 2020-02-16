eb5ad1a089ebbb6d4ef98d4c81c5ddcb751b57ed
olegzaim <olegzaimm@gmail.com>
2020-02-16 10:53:57 +0200

    1. Init IEX cloud service project

---
+++ src/main/java/com/iex/iexcloudservice/IexCloudServiceApplication.java
@@ -0,0 +1,13 @@
+package com.iex.iexcloudservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IexCloudServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IexCloudServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iex/iexcloudservice/IexCloudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iex.iexcloudservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IexCloudServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
